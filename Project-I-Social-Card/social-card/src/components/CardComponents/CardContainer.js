6dffb8d72a3618356793a8f4b373be23cf867d77
Zooheck <davidscottflack@gmail.com>
2018-12-18 17:29:32 -0600

    Setup basic html with components

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+import './CardBanner';
+import './CardContent';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+const CardContainer = () => {
+    return (
+        <a className='card-container'>
+            <CardBanner />
+            <CardContent />
+        </a>
+    );
+}
+
+export default CardContainer;
