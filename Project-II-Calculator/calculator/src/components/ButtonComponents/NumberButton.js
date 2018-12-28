323b17b1c444a05e824cc1562e1bce52fa60a48c
Zooheck <davidscottflack@gmail.com>
2018-12-27 18:04:18 -0600

    Set up skeleton for calculator

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function NumberButton(props) {
+    return (
+        <div className={props.buttonStyle}>{props.text}</div>
+    );
+}
+
+export default NumberButton;
