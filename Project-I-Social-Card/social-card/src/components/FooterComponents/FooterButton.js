fa41aa717498ec2941361b3092b810f92039f99c
Zooheck <davidscottflack@gmail.com>
2019-01-07 16:42:32 -0600

    (social card) set up footer button methods and state

--- Project-I-Social-Card/social-card/src/components/FooterComponents/FooterButton.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/FooterButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function FooterButton(props) {
     return(
-        <button>{props.buttonText}</button>
+        <button onClick={props.onClickHandler}>{props.buttonText} {props.clicks}</button>
     );
 }
 export default FooterButton
